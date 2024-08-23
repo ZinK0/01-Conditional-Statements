db5b9cb28f507412c12af423f0f8067ec96cf7ce
Zin KO <zinko.it@outlook.com>
2024-08-23 15:48:50 +0630

    Added evaluate function for that condition

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -4,3 +4,16 @@ var expression1 = x < 25;
 var expression2 = x > 50;
 
 // TODO: Write Your JavaScript Code Here
+function evaluateExpressions(expression1, expression2) {
+  if (expression1 && expression2) {
+    console.log("True ✅ True ✅");
+  } else if (expression1 && !expression2) {
+    console.log("True ✅ False ❌");
+  } else if (!expression1 && expression2) {
+    console.log("False ❌ True ✅");
+  } else {
+    console.log("False ❌ False ❌");
+  }
+}
+
+evaluateExpressions(expression1, expression2);
